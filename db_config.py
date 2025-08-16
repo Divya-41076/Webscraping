941b1010226ea5d1259a416c7181e2d73c56a8e2
Divyashree Harish <22btrcf011@jainuniversity.ac.in>
2025-08-16 17:08:06 +0530

    Create db_config.py

---
+++ db_config.py
@@ -0,0 +1,13 @@
+# db_config.py
+# update with your own credentials
+
+db_config = {
+    "host": "localhost",
+    "user": "your_mysql_username",
+    "password": "your_mysql_password"
+}
+
+database_name = "job_scraper"
+table_name = "jobs"
+
+search_query = "Python"   # Example search keyword
